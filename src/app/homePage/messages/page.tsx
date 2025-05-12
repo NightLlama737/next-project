5e61152d8a1ad664362de206906c54f59d35d96f
Filip Grafek <grafekfik737@gmail.com>
2025-05-12 22:29:00 +0200

    repaired...

--- src/app/homePage/messages/page.tsx
+++ src/app/homePage/messages/page.tsx
@@ -1,5 +1,4 @@
-import Users from "@/components/(messages)/users";
-import MessageWindow from "@/app/homePage/messages/[messageWindows]/page";
+
 import MessagesPage from "./messagePage";
 
 export default function Home() {
