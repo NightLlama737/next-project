1791d6a795b1d5ab7eac1aa7607f872f2c45e903
Filip Grafek <grafekfik737@gmail.com>
2025-05-12 22:26:29 +0200

    repaired for deploynment

--- src/app/homePage/messages/messagePage.tsx
+++ src/app/homePage/messages/messagePage.tsx
@@ -1,10 +1,8 @@
 "use client"
-import Users from "@/components/(messages)/users";
 import { useState } from "react";
 
 export default function MessagesPage() {
-    const [selectedUserId, setSelectedUserId] = useState<string | null>(null);
-
+    
     return (
         <div style={{
             display: 'flex',
@@ -16,9 +14,9 @@ export default function MessagesPage() {
                 flex: 1,
                 padding: "20px"
             }}>
-                {!selectedUserId && (
+                
                     <div>Select a user to start messaging</div>
-                )}
+             
             </div>
         </div>
     );
